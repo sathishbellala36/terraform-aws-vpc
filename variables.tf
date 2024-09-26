fa0ad5e792c917bca734bd9636090e5b85304bda
sathishbellala36 <sathishbellala36@gmail.com>
2024-09-26 18:07:10 +0530

    user profile

--- variables.tf
+++ variables.tf
@@ -84,3 +84,24 @@ variable "database_subnet_cidrs" {
     default = {}
     
   }
+  variable "public_route_table_tags" {
+    default = {}
+    
+  }
+  variable "private_route_table_tags" {
+    default = {}
+    
+  }
+  variable "database_route_table_tags" {
+    default = {}
+    
+  }
+  variable "is_peering_required" {
+    type = bool
+    default = false
+    
+  }
+  variable "vpc_peering_tags" {
+    default = {}
+    
+  }
